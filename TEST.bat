04203e00cf92af1658c19fa778eddc0b8e2857f2
Naruepanart Siangsanan <33704769+benzsnapr@users.noreply.github.com>
2018-05-21 14:43:14 +0700

    Add files via upload

---
+++ TEST.bat
@@ -0,0 +1,8 @@
+SET URL=www.google.co.th
+cd..
+start firefox -P 01 -no-remote %URL%
+start firefox -P 02 -no-remote %URL%
+start firefox -P 03 -no-remote %URL%
+start firefox -P 04 -no-remote %URL%
+start firefox -P 05 -no-remote %URL%
+pause
